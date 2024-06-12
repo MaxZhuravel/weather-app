ce15101c1357a15fa3af307ce21ad4732e24d1a1
Max <ring.maks@gmail.com>
2024-06-12 23:28:38 +0300

    refactoring getSubtitle

--- src/components/Highlights.js
+++ src/components/Highlights.js
@@ -23,30 +23,22 @@ const Highlights = () => {
         return items;
     }
 
-    const getUnitMeasure = (item) => {
-        const units = {
-            'Pressure': ' gPa',
-            'Wind Status': ' km/h',
-            'Humidity': ' %',
-            'Visibility': ' km',
-            'Clouds': ' %',
+    const getSubtitle = (value, i) => {
+        const subtitleList = {
+            0: getPressure(value),
+            1: getWindDirection(value[1]),
+            2: null,
+            3: getHumidity(value),
+            4: getVisibility(value),
+            5: getClouds(value)
         }
-        return units[item] ? units[item] : null;
+        return subtitleList[i];
     }
 
-    const getValue = (title, source) => {
-        if (title === 'Sunrise & Sunset') {
-            const sunriseDate = new Date(source[0] * 1000);
-            const sunsetDate = new Date(source[1] * 1000);
-            const sunriseTime = sunriseDate.getHours() + ':' + sunriseDate.getMinutes()
-            const sunsetTime = sunsetDate.getHours() + ':' + sunsetDate.getMinutes()
-            return [
-                {imgSrc: '/icons/027-sunrise.png', time: sunriseTime},
-                {imgSrc: '/icons/026-sunset.png', time: sunsetTime}
-            ];
-        } else if (title === 'Wind Status') {
-            return source[0];
-        } else return source;
+    const getPressure = (value) => {
+        if (value < 40) return "Low";
+        if (value > 70) return "High";
+        else return "Normal";
     }
 
     const getWindDirection = (value) => {
@@ -76,39 +68,50 @@ const Highlights = () => {
         return directions[degrees];
     };
 
-    const getSubtitle = (value, i) => {
-        switch (i) {
-            case 0: {
-                if (value < 1000) return 'Low';
-                else if (value > 1013) return 'High';
-                else return 'Normal';
-            }
-            case 1: {
-                return getWindDirection(value[1]);
-            }
-            case 2:
-                return null;
-            case 3: {
-                if (value < 40) return "Low";
-                if (value > 70) return "High";
-                else return "Normal";
-            }
-            case 4: {
-                if (value < 5) return "Low";
-                if (value > 5 && value < 8) return "Average";
-                else return "High";
-            }
-            case 5: {
-                if (value < 10) return "Clear";
-                if (value > 10 && value <= 40) return "Mostly Clear";
-                if (value > 40 && value <= 70) return "Partly Cloudy";
-                if (value > 70 && value <= 90) return "Cloudy";
-                else return "Overcast Clouds"
+    const getHumidity = (value) => {
+        if (value < 40) return "Low";
+        if (value > 70) return "High";
+        else return "Normal";
+    }
 
-            }
-            default:
-                return null;
+    const getVisibility = (value) => {
+        if (value < 5) return "Low";
+        if (value > 5 && value < 8) return "Average";
+        else return "High";
+    }
+
+    const getClouds = (value) => {
+        if (value < 10) return "Clear";
+        if (value > 10 && value <= 40) return "Mostly Clear";
+        if (value > 40 && value <= 70) return "Partly Cloudy";
+        if (value > 70 && value <= 90) return "Cloudy";
+        else return "Overcast Clouds"
+    }
+
+    const getValue = (title, source) => {
+        if (title === 'Sunrise & Sunset') {
+            const sunriseDate = new Date(source[0] * 1000);
+            const sunsetDate = new Date(source[1] * 1000);
+            const sunriseTime = sunriseDate.getHours() + ':' + sunriseDate.getMinutes()
+            const sunsetTime = sunsetDate.getHours() + ':' + sunsetDate.getMinutes()
+            return [
+                {imgSrc: '/icons/027-sunrise.png', time: sunriseTime},
+                {imgSrc: '/icons/026-sunset.png', time: sunsetTime}
+            ];
+        } else if (title === 'Wind Status') {
+            return source[0];
+        } else return source;
+    }
+
+    const getUnitMeasure = (item) => {
+        const units = {
+            'Pressure': ' gPa',
+            'Wind Status': ' km/h',
+            'Humidity': ' %',
+            'Visibility': ' km',
+            'Clouds': ' %',
         }
+        return units[item] ? units[item] : null;
     }
 
     const items = renderHighlightsItems();
