654074ff206fac76e8be7cc8cd9f51921542a1ee
Max <ring.maks@gmail.com>
2024-06-13 10:58:27 +0300

    refactoring of conditional display error message, weather service, conditional creating of weekly forecast

--- src/services/WeatherService.js
+++ src/services/WeatherService.js
@@ -11,49 +11,14 @@ class WeatherService {
     }
 
     getDailyWeatherData = async (city) => {
-        const weatherData = await this.getResource(`https://api.openweathermap.org/data/2.5/weather?&units=metric&appid=b2714a5cbe36acd4448f2292ecddf4ed&q=${city}`);
-
-        return this.transformData(weatherData);
-    }
-
-    getWeekWeatherData = async (city) => {
         const weatherData = await this.getResource(
-            `https://api.openweathermap.org/data/2.5/forecast?units=metric&appid=b2714a5cbe36acd4448f2292ecddf4ed&q=${city}`
+            `https://api.openweathermap.org/data/2.5/weather?&units=metric&appid=b2714a5cbe36acd4448f2292ecddf4ed&q=${city}`
         );
 
-        return this.transformWeekData(weatherData);
-    };
-
-    transformWeekData = (weatherData) => {
-
-        const todayDate = weatherData.list[0].dt_txt.split(' ')[0];
-        const lastDate = weatherData.list[weatherData.list.length - 1].dt_txt.split(' ')[0];
-        const workList = weatherData.list.filter((item) => {
-            if ((!item.dt_txt.includes(todayDate)) && (!item.dt_txt.includes(lastDate))) return item;
-        })
-
-        const days = workList.reduce((acc, current) => {
-            const [date, hour] = current.dt_txt.split(' ');
-            if (!acc[date])
-                acc[date] = { min: current.main.temp, max: current.main.temp, icon: current.weather[0].icon};
-            else {
-                const min = Math.min(acc[date].min, current.main.temp);
-                const max = Math.max(acc[date].max, current.main.temp);
-                const icon = hour === '12:00:00' ? current.weather[0].icon : acc[date].icon;
-                acc[date] = { min, max, icon};
-            }
-            return acc;
-        }, {});
-
-        return Object.entries(days).map(([key, value]) => {
-            const day = new Date(key).toLocaleDateString('en-US', {
-                weekday: 'short',
-            });
-            return { date: key, day, ...value };
-        });
+        return this.transformDailyData(weatherData);
     }
 
-    transformData = (weatherData) => {
+    transformDailyData = (weatherData) => {
         return {
             id: weatherData.id,
             city: weatherData.name,
@@ -71,6 +36,51 @@ class WeatherService {
                 {title: 'Clouds', value: weatherData.clouds.all}
             ],
         }
+    }
+
+    getWeekWeatherData = async (city) => {
+        const weatherData = await this.getResource(
+            `https://api.openweathermap.org/data/2.5/forecast?units=metric&appid=b2714a5cbe36acd4448f2292ecddf4ed&q=${city}`
+        );
+
+        return this.transformWeekData(weatherData);
+    };
+
+    transformWeekData = (weatherData) => {
+
+        const todayDate = weatherData.list[0].dt_txt.split(' ')[0];
+        const lastDate = weatherData.list[weatherData.list.length - 1].dt_txt.split(' ')[0];
+        const workList = this.getWorkList(weatherData, todayDate, lastDate);
+
+        const days = this.getReducedWorkList(workList);
+
+        return Object.entries(days).map(([key, value]) => {
+            const day = new Date(key).toLocaleDateString('en-US', {
+                weekday: 'short',
+            });
+            return { date: key, day, ...value };
+        });
+    }
+
+    getReducedWorkList = (workList) => {
+        return workList.reduce((acc, current) => {
+            const [date, hour] = current.dt_txt.split(' ');
+            if (!acc[date])
+                acc[date] = { min: current.main.temp, max: current.main.temp, icon: current.weather[0].icon};
+            else {
+                const min = Math.min(acc[date].min, current.main.temp);
+                const max = Math.max(acc[date].max, current.main.temp);
+                const icon = hour === '12:00:00' ? current.weather[0].icon : acc[date].icon;
+                acc[date] = { min, max, icon};
+            }
+            return acc;
+        }, {});
+    }
+
+    getWorkList = (weatherData, todayDate, lastDate) => {
+        return weatherData.list.filter((item) => {
+            if ((!item.dt_txt.includes(todayDate)) && (!item.dt_txt.includes(lastDate))) return item;
+        });
     }
 
     bigFirstSymbol = (string) => {
